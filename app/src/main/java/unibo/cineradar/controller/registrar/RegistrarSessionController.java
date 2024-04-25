e162a2a8d20768ce2bbf14ff41e8a5637748a8bb
Luca Casadei <casadeiluca30@gmail.com>
2024-04-25 11:37:00 +0200

    Now the cinema query fetches the number of users that have been registered.

--- app/src/main/java/unibo/cineradar/controller/registrar/RegistrarSessionController.java
+++ app/src/main/java/unibo/cineradar/controller/registrar/RegistrarSessionController.java
@@ -2,8 +2,11 @@ package unibo.cineradar.controller.registrar;
 
 import unibo.cineradar.controller.SessionController;
 import unibo.cineradar.controller.SessionControllerImpl;
+import unibo.cineradar.model.card.CardReg;
 import unibo.cineradar.model.cinema.Cinema;
 import unibo.cineradar.model.context.registrar.RegistrarContext;
+
+import java.time.LocalDate;
 
 /**
  * The controller class for the registrar.
@@ -28,5 +31,21 @@ public final class RegistrarSessionController extends SessionControllerImpl {
      */
     public Cinema getCinemaDetails() {
         return registrarContext.getCinema();
+    }
+
+    /**
+     * Creates a card and inserts it into the DB.
+     *
+     * @param username    The username of the card.
+     * @param renewalDate The renewal date of the card.
+     * @param cardNr      The number of the card.
+     * @param cinemaNr    The cinema number of the card.
+     * @return True if the insertion was successful, false otherwise.
+     */
+    public boolean insertCard(final String username,
+                              final LocalDate renewalDate,
+                              final int cardNr,
+                              final int cinemaNr) {
+        return registrarContext.registerCard(new CardReg(username, renewalDate, cinemaNr, cardNr));
     }
 }
--- app/src/main/java/unibo/cineradar/model/cinema/Cinema.java
+++ app/src/main/java/unibo/cineradar/model/cinema/Cinema.java
@@ -3,12 +3,19 @@ package unibo.cineradar.model.cinema;
 /**
  * Represents a cinema in the model.
  *
- * @param codice The main ID of the cinema.
- * @param nome   The name of the cinema.
- * @param indVia The street of the cinema.
- * @param indCAP The postal code of the cinema.
- * @param citta  The town of the cinema.
- * @param civico The street number of the cinema.
+ * @param codice   The main ID of the cinema.
+ * @param nome     The name of the cinema.
+ * @param indVia   The street of the cinema.
+ * @param indCAP   The postal code of the cinema.
+ * @param citta    The town of the cinema.
+ * @param civico   The street number of the cinema.
+ * @param cardedNr The number of carded users of this cinema.
  */
-public record Cinema(int codice, String nome, String indVia, String indCAP, String citta, int civico) {
+public record Cinema(int codice,
+                     String nome,
+                     String indVia,
+                     String indCAP,
+                     String citta,
+                     int civico,
+                     int cardedNr) {
 }
