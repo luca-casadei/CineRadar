f992f282488bf19c65c62e8af6dde23963a1faf0
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-04-25 09:17:09 +0200

    Change UserHomePageView layout to CardLayout.

--- app/src/main/java/unibo/cineradar/view/homepage/UserHomePageView.java
+++ app/src/main/java/unibo/cineradar/view/homepage/UserHomePageView.java
@@ -11,10 +11,7 @@ import unibo.cineradar.view.utilities.ViewUtilities;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
-import java.awt.BorderLayout;
-import java.awt.Color;
-import java.awt.FlowLayout;
-import java.awt.Image;
+import java.awt.*;
 import java.util.List;
 import javax.swing.ImageIcon;
 
@@ -23,8 +20,15 @@ import javax.swing.ImageIcon;
  */
 public final class UserHomePageView extends CineRadarViewFrameImpl {
 
+    private static final String FILM_NAME = "film";
+    private static final String SERIE_NAME = "serie";
+    private static final String REVIEW_NAME = "review";
+    private static final String PROFILE_NAME = "profile";
+
     private final List<JPanel> jPanelList;
-    private final JPanel contentPane = new JPanel(new BorderLayout());
+    private final ViewContext context;
+    private CardLayout cards;
+    private JPanel cardPane;
 
     /**
      * Constructor of the user homepage view.
@@ -34,6 +38,7 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
     public UserHomePageView(final ViewContext currentSessionContext) {
         super();
 
+        this.context = currentSessionContext;
         this.getMainFrame().setTitle("Benvenuto "
                 + currentSessionContext.getController().getAccountDetails().get(0));
 
@@ -47,7 +52,62 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
         this.setInternalComponents();
     }
 
-    private void setInternalComponents() {
+    private void setInternalComponents(){
+        final JPanel contentPane = new JPanel(new BorderLayout());
+        contentPane.add(getNavBar(), BorderLayout.NORTH);
+        this.getMainFrame().setContentPane(contentPane);
+
+        this.cards = new CardLayout();
+        this.cardPane = new JPanel(cards);
+        contentPane.add(cardPane, BorderLayout.CENTER);
+
+        //Home panel
+        cardPane.add(FILM_NAME, new UserFilmView(this.context));
+        cardPane.add(SERIE_NAME, new UserSerieView(this.context));
+        cardPane.add(REVIEW_NAME, new UserReviewView(this.context));
+        cardPane.add(PROFILE_NAME, new UserProfileView(this.context));
+    }
+
+    private JPanel getNavBar(){
+        final FlowLayout navLayout = new FlowLayout();
+        navLayout.setAlignment(FlowLayout.LEFT);
+        final JPanel navBar = new JPanel(navLayout);
+        final JButton filmButton = getGotoFilmButton();
+        final JButton serieButton = getGotoSerieButton();
+        final JButton reviewButton = getGotoReviewButton();
+        final JButton profileButton = getGotoProfileButton();
+        navBar.add(filmButton);
+        navBar.add(serieButton);
+        navBar.add(reviewButton);
+        navBar.add(profileButton);
+        return navBar;
+    }
+
+    private JButton getGotoFilmButton(){
+        final JButton filmButton = new JButton("FILM");
+        filmButton.addActionListener(e -> cards.show(cardPane, FILM_NAME));
+        return filmButton;
+    }
+
+    private JButton getGotoSerieButton(){
+        final JButton serieButton = new JButton("SERIE");
+        serieButton.addActionListener(e -> cards.show(cardPane, SERIE_NAME));
+        return serieButton;
+    }
+
+    private JButton getGotoReviewButton(){
+        final JButton reviewButton = new JButton("LE MIE RECENSIONI");
+        reviewButton.addActionListener(e -> cards.show(cardPane, REVIEW_NAME));
+        return reviewButton;
+    }
+
+    private JButton getGotoProfileButton(){
+        final JButton profileButton = new JButton("PROFILO");
+        profileButton.addActionListener(e -> cards.show(cardPane, PROFILE_NAME));
+        return profileButton;
+    }
+
+    /*private void setInternalComponents() {
         this.getMainFrame().setContentPane(contentPane);
         // Navigation bar on top
         final FlowLayout navLayout = new FlowLayout();
@@ -90,5 +150,5 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
         this.getMainFrame().revalidate();
         this.contentPane.revalidate();
         this.contentPane.repaint();
-    }
+    }*/
 }
