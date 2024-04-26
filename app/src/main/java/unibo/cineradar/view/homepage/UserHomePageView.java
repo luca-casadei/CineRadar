39592d8f3f086d890a416c48a1600434f30fb674
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-04-26 11:24:05 +0200

    Fixed all checkstyle and PMD errors.

--- app/src/main/java/unibo/cineradar/view/homepage/UserHomePageView.java
+++ app/src/main/java/unibo/cineradar/view/homepage/UserHomePageView.java
@@ -6,14 +6,12 @@ import unibo.cineradar.view.homepage.user.UserFilmView;
 import unibo.cineradar.view.homepage.user.UserProfileView;
 import unibo.cineradar.view.homepage.user.UserReviewView;
 import unibo.cineradar.view.homepage.user.UserSerieView;
-import unibo.cineradar.view.utilities.ViewUtilities;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
-import javax.swing.JLabel;
-import java.awt.*;
-import java.util.List;
-import javax.swing.ImageIcon;
+import java.awt.FlowLayout;
+import java.awt.CardLayout;
+import java.awt.BorderLayout;
 
 /**
  * HomePage view of the user.
@@ -25,7 +23,6 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
     private static final String REVIEW_NAME = "review";
     private static final String PROFILE_NAME = "profile";
 
-    private final List<JPanel> jPanelList;
     private final ViewContext context;
     private CardLayout cards;
     private JPanel cardPane;
@@ -42,17 +39,10 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
         this.getMainFrame().setTitle("Benvenuto "
                 + currentSessionContext.getController().getAccountDetails().get(0));
 
-        this.jPanelList = List.of(
-                new UserFilmView(currentSessionContext),
-                new UserProfileView(currentSessionContext),
-                new UserReviewView(currentSessionContext),
-                new UserSerieView(currentSessionContext)
-        );
-
         this.setInternalComponents();
     }
 
-    private void setInternalComponents(){
+    private void setInternalComponents() {
         final JPanel contentPane = new JPanel(new BorderLayout());
         contentPane.add(getNavBar(), BorderLayout.NORTH);
         this.getMainFrame().setContentPane(contentPane);
@@ -68,7 +58,7 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
         cardPane.add(PROFILE_NAME, new UserProfileView(this.context));
     }
 
-    private JPanel getNavBar(){
+    private JPanel getNavBar() {
         final FlowLayout navLayout = new FlowLayout();
         navLayout.setAlignment(FlowLayout.LEFT);
         final JPanel navBar = new JPanel(navLayout);
@@ -83,72 +73,27 @@ public final class UserHomePageView extends CineRadarViewFrameImpl {
         return navBar;
     }
 
-    private JButton getGotoFilmButton(){
+    private JButton getGotoFilmButton() {
         final JButton filmButton = new JButton("FILM");
         filmButton.addActionListener(e -> cards.show(cardPane, FILM_NAME));
         return filmButton;
     }
 
-    private JButton getGotoSerieButton(){
+    private JButton getGotoSerieButton() {
         final JButton serieButton = new JButton("SERIE");
         serieButton.addActionListener(e -> cards.show(cardPane, SERIE_NAME));
         return serieButton;
     }
 
-    private JButton getGotoReviewButton(){
+    private JButton getGotoReviewButton() {
         final JButton reviewButton = new JButton("LE MIE RECENSIONI");
         reviewButton.addActionListener(e -> cards.show(cardPane, REVIEW_NAME));
         return reviewButton;
     }
 
-    private JButton getGotoProfileButton(){
+    private JButton getGotoProfileButton() {
         final JButton profileButton = new JButton("PROFILO");
         profileButton.addActionListener(e -> cards.show(cardPane, PROFILE_NAME));
         return profileButton;
     }
-
-    /*private void setInternalComponents() {
-        this.getMainFrame().setContentPane(contentPane);
-        // Navigation bar on top
-        final FlowLayout navLayout = new FlowLayout();
-        navLayout.setAlignment(FlowLayout.LEFT);
-        final JPanel topNavPanel = new JPanel(navLayout);
-        topNavPanel.setBackground(Color.GRAY);
-        final Image logoImage = new ImageIcon(ViewUtilities.getResourceURL(
-                ViewUtilities.DEFAULT_IMAGE_PATH + "/logo.png"))
-                .getImage().getScaledInstance(300, 50, Image.SCALE_SMOOTH);
-        final JLabel imageLabel = new JLabel();
-        imageLabel.setIcon(new ImageIcon(logoImage));
-        topNavPanel.add(imageLabel);
-        final JButton filmBtn = new JButton("FILM");
-        topNavPanel.add(filmBtn);
-        filmBtn.addActionListener(e -> displayHide(UserFilmView.class.getCanonicalName()));
-        final JButton serieBtn = new JButton("SERIE TV");
-        topNavPanel.add(serieBtn);
-        serieBtn.addActionListener(e -> displayHide(UserSerieView.class.getCanonicalName()));
-        final JButton reviewBtn = new JButton("LE MIE RECENSIONI");
-        topNavPanel.add(reviewBtn);
-        reviewBtn.addActionListener(e -> displayHide(UserReviewView.class.getCanonicalName()));
-        final JButton profileBtn = new JButton("PROFILO");
-        topNavPanel.add(profileBtn);
-        profileBtn.addActionListener(e -> displayHide(UserProfileView.class.getCanonicalName()));
-
-        this.contentPane.add(topNavPanel, BorderLayout.NORTH);
-
-        this.contentPane.add(this.jPanelList.get(0), BorderLayout.CENTER);
-    }
-
-    private void displayHide(final String toShow) {
-        for (final JPanel jp : jPanelList) {
-            final boolean visibilize = jp.getClass().getCanonicalName().equals(toShow);
-            if (!visibilize) {
-                this.contentPane.remove(jp);
-            } else {
-                this.contentPane.add(jp, BorderLayout.CENTER);
-            }
-        }
-        this.getMainFrame().revalidate();
-        this.contentPane.revalidate();
-        this.contentPane.repaint();
-    }*/
 }
