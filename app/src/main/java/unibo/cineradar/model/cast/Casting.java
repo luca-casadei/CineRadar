fae40a5572cf6865d5e889999d5e0eb67524a625
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-05-08 10:49:10 +0200

    Create Casting Class

---
+++ app/src/main/java/unibo/cineradar/model/cast/Casting.java
@@ -0,0 +1,10 @@
+package unibo.cineradar.model.cast;
+
+/**
+ * Represents a casting, associating an ID with a name.
+ *
+ * @param id   The unique identifier of the casting.
+ * @param name The name of the casting.
+ */
+public record Casting(int id, String name) {
+}
