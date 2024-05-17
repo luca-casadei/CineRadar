452c26bff7deafba4c7fc1bfb2d6f4ea01c3f89d
Francesco Pazzaglia <francesco.pazzaglia3@studio.unibo.it>
2024-05-17 12:59:45 +0200

    Added age limit filter in UserFilmView and UserSerieView.

--- app/src/main/java/unibo/cineradar/view/homepage/user/UserFilmView.java
+++ app/src/main/java/unibo/cineradar/view/homepage/user/UserFilmView.java
@@ -2,10 +2,15 @@ package unibo.cineradar.view.homepage.user;
 
 import unibo.cineradar.view.ViewContext;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
-import javax.swing.JTable;
+import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextArea;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.io.Serial;
 
@@ -25,7 +30,6 @@ public final class UserFilmView extends UserPanel {
      */
     public UserFilmView(final ViewContext currentSessionContext) {
         super(currentSessionContext);
-        // Adds the welcome label to the view
         final JLabel welcomeLabel = new JLabel("Benvenuto "
                 + currentSessionContext.getController().getAccountDetails().get(0)
                 + " nella pagina dei film.");
@@ -33,10 +37,38 @@ public final class UserFilmView extends UserPanel {
         welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
         this.add(welcomeLabel, BorderLayout.NORTH);
 
-        // Adds the film table to the view
         final JTable filmTable = super.createFilmTable();
         final JScrollPane scrollPane = new JScrollPane(filmTable);
         this.add(scrollPane, BorderLayout.CENTER);
-    }
 
+        final JPanel filterPanel = new JPanel();
+        filterPanel.setLayout(new FlowLayout());
+
+        final JCheckBox filterCheckbox = new JCheckBox("Filtra per eta'");
+        filterPanel.add(filterCheckbox);
+
+        final JTextArea ageTextArea = new JTextArea(1, 10);
+        ageTextArea.setEnabled(false);
+        filterPanel.add(new JLabel("Eta' limite:"));
+        filterPanel.add(ageTextArea);
+
+        final JButton filterButton = new JButton("Applica filtro");
+        filterButton.setEnabled(false);
+        filterPanel.add(filterButton);
+
+        this.add(filterPanel, BorderLayout.SOUTH);
+
+        filterCheckbox.addActionListener(e -> {
+            final boolean selected = filterCheckbox.isSelected();
+            ageTextArea.setEnabled(selected);
+            filterButton.setEnabled(selected);
+        });
+
+        /*filterButton.addActionListener(e -> {
+            final String text = ageTextArea.getText();
+            if (!text.isEmpty()) {
+                // TODO: chiamata a DB
+            }
+        });*/
+    }
 }
--- app/src/main/java/unibo/cineradar/view/homepage/user/UserSerieView.java
+++ app/src/main/java/unibo/cineradar/view/homepage/user/UserSerieView.java
@@ -2,11 +2,15 @@ package unibo.cineradar.view.homepage.user;
 
 import unibo.cineradar.view.ViewContext;
 
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
 import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 import java.awt.Font;
 
 /**
@@ -33,6 +37,36 @@ public final class UserSerieView extends UserPanel {
         final JScrollPane scrollPane = new JScrollPane(serieTable);
         mainPanel.add(welcomeLabel, BorderLayout.NORTH);
         mainPanel.add(scrollPane, BorderLayout.CENTER);
+
+        final JPanel filterPanel = new JPanel();
+        filterPanel.setLayout(new FlowLayout());
+
+        final JCheckBox filterCheckbox = new JCheckBox("Filtra per eta'");
+        filterPanel.add(filterCheckbox);
+
+        final JTextArea ageTextArea = new JTextArea(1, 10);
+        ageTextArea.setEnabled(false);
+        filterPanel.add(new JLabel("Eta' limite:"));
+        filterPanel.add(ageTextArea);
+
+        final JButton filterButton = new JButton("Applica filtro");
+        filterButton.setEnabled(false);
+        filterPanel.add(filterButton);
+
+        mainPanel.add(filterPanel, BorderLayout.SOUTH);
+
+        filterCheckbox.addActionListener(e -> {
+            final boolean selected = filterCheckbox.isSelected();
+            ageTextArea.setEnabled(selected);
+            filterButton.setEnabled(selected);
+        });
+
+        /*filterButton.addActionListener(e -> {
+            final String text = ageTextArea.getText();
+            if (!text.isEmpty()) {
+                // TODO: chiamata a DB
+            }
+        });*/
 
         add(mainPanel);
         setVisible(true);
