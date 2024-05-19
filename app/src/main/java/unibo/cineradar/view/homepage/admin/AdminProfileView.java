2950b24e5edd362aaa842469f9de777a5f4ba08d
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-05-19 20:53:39 +0200

    Changed the display of administrator profile information

--- app/src/main/java/unibo/cineradar/view/homepage/admin/AdminProfileView.java
+++ app/src/main/java/unibo/cineradar/view/homepage/admin/AdminProfileView.java
@@ -4,10 +4,15 @@ import unibo.cineradar.model.utente.Account;
 import unibo.cineradar.model.utente.Administrator;
 import unibo.cineradar.view.ViewContext;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.BorderFactory;
-import java.awt.GridLayout;
+import javax.swing.border.EmptyBorder;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.BorderLayout;
 import java.io.Serial;
 
@@ -18,7 +23,7 @@ import java.io.Serial;
  */
 public final class AdminProfileView extends AdminPanel {
     @Serial
-    private static final long serialVersionUID = -6223416854460075865L;
+    private static final long serialVersionUID = -7108264925073091345L;
 
     /**
      * Constructor of the user profile view.
@@ -28,30 +33,51 @@ public final class AdminProfileView extends AdminPanel {
     public AdminProfileView(final ViewContext currentSessionContext) {
         super(currentSessionContext);
         final Account account = currentSessionContext.getController().getAccount();
-        if (account instanceof Administrator) {
-            final Administrator admin;
-            admin = (Administrator) account;
-            final JPanel userInfoPanel = this.getUserInfoPanel(admin);
+        if (account instanceof Administrator admin) {
+            final JPanel mainPanel = new JPanel(new BorderLayout());
+            mainPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+            final JLabel welcomeLabel = new JLabel("Benvenuto "
+                    + currentSessionContext.getController().getAccountDetails().get(0)
+                    + " nella pagina di profilo.");
+            welcomeLabel.setFont(new Font("Arial", Font.BOLD, 24));
+            welcomeLabel.setHorizontalAlignment(JLabel.CENTER);
+            mainPanel.add(welcomeLabel, BorderLayout.NORTH);
+
+            final JPanel userInfoPanel = createAdminInfoPanel(admin);
+            mainPanel.add(userInfoPanel, BorderLayout.CENTER);
+
             this.setLayout(new BorderLayout());
-            this.add(userInfoPanel, BorderLayout.CENTER);
+            this.add(mainPanel, BorderLayout.CENTER);
         }
     }
 
-    private JPanel getUserInfoPanel(final Administrator admin) {
-        final JPanel userInfoPanel = new JPanel();
-        userInfoPanel.setLayout(new GridLayout(0, 1, 10, 10));
-        userInfoPanel.setBorder(BorderFactory.createEmptyBorder(20, 20, 20, 20));
+    private JPanel createAdminInfoPanel(final Administrator admin) {
+        final JPanel adminInfoPanel = new JPanel();
+        adminInfoPanel.setLayout(new BoxLayout(adminInfoPanel, BoxLayout.Y_AXIS));
+        adminInfoPanel.setBorder(new EmptyBorder(20, 50, 20, 50));
 
-        final JLabel nameLabel = new JLabel("Nome: " + admin.getName());
-        final JLabel surnameLabel = new JLabel("Cognome: " + admin.getLastName());
-        final JLabel usernameLabel = new JLabel("Username: " + admin.getUsername());
-        nameLabel.setHorizontalAlignment(JLabel.CENTER);
-        surnameLabel.setHorizontalAlignment(JLabel.CENTER);
-        usernameLabel.setHorizontalAlignment(JLabel.CENTER);
-        userInfoPanel.add(nameLabel);
-        userInfoPanel.add(surnameLabel);
-        userInfoPanel.add(usernameLabel);
-        return userInfoPanel;
+        final JLabel adminNameLabel = createLabel("Nome: " + admin.getName());
+        final JLabel adminSurnameLabel = createLabel("Cognome: " + admin.getLastName());
+        final JLabel adminUsernameLabel = createLabel("Username: " + admin.getUsername());
+        final JLabel adminPhoneNumberLabel = createLabel("Telefono: " + admin.getPhoneNumber());
+
+        adminInfoPanel.add(adminNameLabel);
+        adminInfoPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        adminInfoPanel.add(adminSurnameLabel);
+        adminInfoPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        adminInfoPanel.add(adminUsernameLabel);
+        adminInfoPanel.add(Box.createRigidArea(new Dimension(0, 20)));
+        adminInfoPanel.add(adminPhoneNumberLabel);
+
+        return adminInfoPanel;
+    }
+
+    private JLabel createLabel(final String text) {
+        final JLabel label = new JLabel(text);
+        label.setFont(new Font("Arial", Font.PLAIN, 18));
+        label.setHorizontalAlignment(JLabel.CENTER);
+        label.setAlignmentX(Component.CENTER_ALIGNMENT);
+        return label;
     }
 }
 
